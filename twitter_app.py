3a164126ed511052459048e5a7c32f423ec3d868
paul <eduardo-paul@outlook.com>
2020-07-21 17:17:47 -0300

    simplify a bit

--- twitter_app.py
+++ twitter_app.py
@@ -1,26 +1,20 @@
 from requests import get
 from requests_oauthlib import OAuth1
-from json import loads
+from json import load, loads
 
-search_term = 'economics'
-twitter_request = f'https://stream.twitter.com/1.1/statuses/filter.json?track={search_term}'
+SEARCH_TERM = 'brasil'
+LANGUAGE = 'pt'
+twitter_request = f'https://stream.twitter.com/1.1/statuses/filter.json?track={SEARCH_TERM}&language={LANGUAGE}'
 
-with open('credentials.json', 'r') as credentials:
-    creds = loads(
-        credentials.read()
-    ).values()
+with open('credentials.json', 'r') as file:
+    credentials = load(file).values()
 
 response = get(
     twitter_request,
-    auth = OAuth1(*creds),
+    auth=OAuth1(*credentials),
     stream=True
 )
 
-count = 0
 for chunk in response.iter_content(chunk_size=None):
     tweet = loads(chunk)['text']
     print(tweet)
-    if count < 10:
-        count += 1
-    else:
-        break
