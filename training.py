13779ba02468585c9f0f3563d13b256c17c4323b
Eduardo Paul <ecpaul@protonmail.com>
2020-08-18 17:49:44 -0300

    First trained model. Just raw tokenization.

--- training.py
+++ training.py
@@ -1,55 +1,58 @@
 #%%
-from re import compile
-import pandas as pd
+import pickle
+from random import shuffle
+from pandas import read_csv
+from nltk.tokenize import word_tokenize
+from nltk import FreqDist
+from nltk.classify.naivebayes import NaiveBayesClassifier
+from nltk.classify import accuracy
 
-#%%
-data = pd.read_csv(
+data = read_csv(
     'data/train.csv',
     names=['sentiment', 'id', 'date', 'query', 'handle', 'tweet']
+)[:10000] ####### CHANGE THIS!!!!!!
+
+sentiments = data['sentiment']
+sentiments = sentiments.replace(to_replace=4, value=1)
+
+tweets = data['tweet']
+tweets = tweets.apply(
+    lambda tweet: word_tokenize(tweet.lower())
 )
 
+all_words = FreqDist(
+    word.lower()
+    for tweet in tweets
+    for word in tweet
+)
+
+NUM_OF_FEATURES = 2000
+word_features = list(all_words)[:NUM_OF_FEATURES]
+
+def tweet_features(tweet):
+    tweet_words = set(tweet)
+    features = {}
+    for word in word_features:
+        features[f'contains({word})'] = (word in tweet_words)
+    return features
+
+featureset = [
+    (tweet_features(tweet), sentiment)
+    for tweet, sentiment in zip(tweets, sentiments)
+]
+
+shuffle(featureset)
+
+TRAIN_SET_FRAC = 0.8
+train_set_len = round(len(featureset) * TRAIN_SET_FRAC)
+test_set_len = len(featureset) - train_set_len
+
+train_set = featureset[:train_set_len]
+test_set = featureset[train_set_len:]
+
+classifier = NaiveBayesClassifier.train(train_set)
+with open('trained_classifier.pkl', 'wb') as file:
+    pickle.dump(classifier, file)
+
+print(accuracy(classifier, test_set))
 #%%
-data.shape
-
-#%%
-sentiments = data['sentiment']
-tweets = data['tweet'].str.lower()
-
-# %% [markdown]
-## Dealing with emails.
-# It's extremely hard to write a regexp to catch every possible email, but this simple model should probably work on most relevant instances.
-
-#%%
-email_regexp = compile(r'[^\s]+@[^\s]+\.[^\s]+(?:\.[^\s]+)*')
-print(f'number of emails in the data set: {tweets.str.contains(email_regexp).sum()}')
-tweets = tweets.str.replace(email_regexp, 'EMAIL')
-
-#%% [markdown]
-## Dealing with user handles.
-# By twitter's rules, user handles have to begin with an '@' followed by letters, numbers and underscores.
-
-#%%
-user_handle_regexp = compile(r'@\w+')
-print(f'number of user handles in the data set: {tweets.str.contains(user_handle_regexp).sum()}')
-tweets = tweets.str.replace(user_handle_regexp, 'USER_HANDLE')
-
-# %% [markdown]
-## Dealing with URLs.
-# Many times users include URLs in their tweets. They may come in several forms, many time including 'http', 'www', '.com', or the like.
-
-#%%
-url_regexp = compile(r'(http[^\s]+|www\.[\w/\.-]+|\w+\.(?:com|org|net))')
-print(f'number of urls in the data set: {tweets.str.contains(url_regexp).sum()}')
-tweets = tweets.str.replace(url_regexp, 'URL')
-
-# %% [markdown]
-## Dealing with repeated letters.
-# If a letter appears more than two times in a row, the sequence is replaced with only two repetitions
-
-#%%
-repeated_letters_regexp = compile(r'(\w*([a-zA-Z])\2{2,}\w*)')
-repeat = tweets.str.extractall(repeated_letters_regexp)
-repeat
-
-#%%
-repeat.sample(n=100).values
