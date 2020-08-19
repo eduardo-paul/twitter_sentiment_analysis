5f3dc43e4c618f30b2e7b94779e25d7a8ec46534
Eduardo Paul <ecpaul@protonmail.com>
2020-08-19 11:46:35 -0300

    Included regexps.

--- training.py
+++ training.py
@@ -1,25 +1,60 @@
-#%%
 import pickle
 from random import shuffle
+from re import compile
 from pandas import read_csv
 from nltk.tokenize import word_tokenize
 from nltk import FreqDist
 from nltk.classify.naivebayes import NaiveBayesClassifier
 from nltk.classify import accuracy
 
+### Data ingestion
+
 data = read_csv(
     'data/train.csv',
     names=['sentiment', 'id', 'date', 'query', 'handle', 'tweet']
-)[:10000] ####### CHANGE THIS!!!!!!
+)[:10000] ####### FOR TESTING PURPOSES!!!
 
 sentiments = data['sentiment']
-sentiments = sentiments.replace(to_replace=4, value=1)
+sentiments = sentiments.replace(to_replace=4, value=1) # 0->negative, 1->positive
+tweets = data['tweet'].str.lower()
 
-tweets = data['tweet']
-tweets = tweets.apply(
-    lambda tweet: word_tokenize(tweet.lower())
+### Data processing
+
+## Dealing with emails.
+# It's extremely hard to write a regexp to catch every possible email, but this simple model should probably work on most relevant instances.
+
+email_regexp = compile(r'[^\s]+@[^\s]+\.[^\s]+(?:\.[^\s]+)*')
+print(f'number of emails in the data set: {tweets.str.contains(email_regexp).sum()}')
+tweets = tweets.str.replace(email_regexp, 'EMAIL')
+
+## Dealing with user handles.
+# By twitter's rules, user handles have to begin with an '@' followed by letters, numbers and underscores.
+
+user_handle_regexp = compile(r'@\w+')
+print(f'number of user handles in the data set: {tweets.str.contains(user_handle_regexp).sum()}')
+tweets = tweets.str.replace(user_handle_regexp, 'USER_HANDLE')
+
+## Dealing with URLs.
+# Many times users include URLs in their tweets. They may come in several forms, many time including 'http', 'www', '.com', or the like.
+
+url_regexp = compile(r'(http[^\s]+|www\.[\w/\.-]+|\w+\.(?:com|org|net))')
+print(f'number of urls in the data set: {tweets.str.contains(url_regexp).sum()}')
+tweets = tweets.str.replace(url_regexp, 'URL')
+
+## Dealing with repeated letters.
+# If a letter appears more than two times in a row, the sequence is replaced with only two repetitions
+
+repeated_letters_regexp = compile(r'([a-zA-Z])\1{2,}')
+tweets = tweets.str.replace(
+    repeated_letters_regexp,
+    lambda m: 2*m.group(1)
 )
+# This data set presents some uncompiled html code. Here we are replacing the &quot; sequence by " so we can we rid of it when tokenizing.
+tweets = tweets.str.replace('&quot;', '"')
 
+tweets = tweets.apply(word_tokenize)
+
+### Training.
 all_words = FreqDist(
     word.lower()
     for tweet in tweets
@@ -51,8 +86,8 @@ train_set = featureset[:train_set_len]
 test_set = featureset[train_set_len:]
 
 classifier = NaiveBayesClassifier.train(train_set)
+
 with open('trained_classifier.pkl', 'wb') as file:
     pickle.dump(classifier, file)
 
 print(accuracy(classifier, test_set))
-#%%
